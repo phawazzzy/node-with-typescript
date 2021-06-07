d0e937985a08aa0996fbce1859b151e8aa418cfa
Fawas kareem <phawazzzy@gmail.com>
2021-06-07 16:47:32 +0100

    configured all the user route

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -8,6 +8,32 @@ export class UsersRoutes extends CommonRoutesConfig {
 
     configureRoutes() {
         // implementation here
+        this.app.route("/users")
+            .get((req: express.Request, res: express.Response) => {
+                res.status(200).json({ user: "list of users" });
+             })
+            .post((req: express.Request, res: express.Response) => {
+                res.status(200).json({ user: "new user" });
+             });
+        
+        this.app.route("/users/:userId")
+            .all((req: express.Request, res: express.Response, next: express.NextFunction) => {
+
+              next();
+            })
+            .get((req: express.Request, res: express.Response) => {
+                res.status(200).send(`GET requested for id ${req.params.userId}`)
+            })
+            .put((req: express.Request, res: express.Response) => {
+                res.status(200).send(`PUT requested for id ${req.params.userId}`);
+            })
+            .patch((req: express.Request, res: express.Response) => {
+                res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+            })
+            .delete((req: express.Request, res: express.Response) => {
+                res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+            });
+        
         return this.app;
     }
 }
